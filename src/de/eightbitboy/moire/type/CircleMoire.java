6e78e9a905c0f9de19080d6a72a822dee2cc23d4
Stefan Haertel <haertel.dev@gmail.com>
2014-09-07 13:24:18 +0200

    generate moire

--- src/de/eightbitboy/moire/type/CircleMoire.java
+++ src/de/eightbitboy/moire/type/CircleMoire.java
@@ -2,11 +2,15 @@ package de.eightbitboy.moire.type;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint.Style;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
 
 public class CircleMoire extends BasicMoire {
+
+	public static int SPACE = 7;
+	public static int OFFSET = 30;
 
 	public CircleMoire(Context context) {
 		super(context);
@@ -24,9 +28,19 @@ public class CircleMoire extends BasicMoire {
 		int height = canvas.getHeight();
 
 		ShapeDrawable circle = new ShapeDrawable(new OvalShape());
+		circle.getPaint().setAntiAlias(true);
+		circle.getPaint().setStyle(Style.STROKE);
+		circle.getPaint().setStrokeWidth(0);
 		circle.getPaint().setColor(0xff0000ff);
-		circle.setBounds(0, 0, width, height);
 
-		circle.draw(canvas);
+		for (int i = 0; i < width; i += SPACE) {
+			circle.setBounds(0 + i, 0 + i, width - i, height - i);
+			circle.draw(canvas);
+		}
+
+		for (int i = 0; i < width; i += SPACE) {
+			circle.setBounds(0 + i, 0 + OFFSET + i, width - i, height + OFFSET - i);
+			circle.draw(canvas);
+		}
 	}
 }
