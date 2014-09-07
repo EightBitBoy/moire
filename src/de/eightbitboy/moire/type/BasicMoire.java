04f93678142172b673d54e3e16236bd424ac9b90
Stefan Haertel <haertel.dev@gmail.com>
2014-09-07 12:38:21 +0200

    add moire classes

---
+++ src/de/eightbitboy/moire/type/BasicMoire.java
@@ -0,0 +1,11 @@
+package de.eightbitboy.moire.type;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+public class BasicMoire extends ImageView {
+
+	public BasicMoire(Context context) {
+		super(context);
+	}
+}
---
+++ src/de/eightbitboy/moire/type/CircleMoire.java
@@ -0,0 +1,10 @@
+package de.eightbitboy.moire.type;
+
+import android.content.Context;
+
+public class CircleMoire extends BasicMoire {
+
+	public CircleMoire(Context context) {
+		super(context);
+	}
+}
