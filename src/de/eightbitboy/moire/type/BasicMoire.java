7cd4d6946aa4326a105d64b89e8323b43c6996e0
Stefan Haertel <haertel.dev@gmail.com>
2014-09-07 13:07:27 +0200

    draw a simple circle

--- src/de/eightbitboy/moire/type/BasicMoire.java
+++ src/de/eightbitboy/moire/type/BasicMoire.java
@@ -1,11 +1,16 @@
 package de.eightbitboy.moire.type;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class BasicMoire extends ImageView {
 
 	public BasicMoire(Context context) {
 		super(context);
+	}
+
+	public BasicMoire(Context context, AttributeSet attrs) {
+		super(context, attrs);
 	}
 }
--- src/de/eightbitboy/moire/type/CircleMoire.java
+++ src/de/eightbitboy/moire/type/CircleMoire.java
@@ -1,10 +1,32 @@
 package de.eightbitboy.moire.type;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.util.AttributeSet;
 
 public class CircleMoire extends BasicMoire {
 
 	public CircleMoire(Context context) {
 		super(context);
+	}
+
+	public CircleMoire(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+
+		int width = canvas.getWidth();
+		int height = canvas.getHeight();
+
+		ShapeDrawable circle = new ShapeDrawable(new OvalShape());
+		circle.getPaint().setColor(0xff0000ff);
+		circle.setBounds(0, 0, width, height);
+
+		circle.draw(canvas);
 	}
 }
